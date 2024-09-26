ba47499ab806200f474282e5ea426edee2830cdb
Nicholas <nicholas.griffin@bbc.co.uk>
2024-09-26 22:45:33 +0100

    chore: adding a bit of style

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from 'next';
 
 import './globals.css';
 
+import { Avatar, AvatarImage, AvatarFallback } from '@/components/ui/avatar';
+
 export const metadata: Metadata = {
   title: 'NG AI App - Image to Website',
   description: 'A side project to convert images to websites',
@@ -14,7 +16,29 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className="safe-area-padding">
+        <div className="min-h-screen bg-background p-0">
+          <header className="w-full py-4 px-4 sm:px-6 lg:px-8">
+            <div className="flex items-center">
+              <div className="flex items-center flex-shrink-0 text-primary mr-6">
+                <Avatar>
+                  <AvatarImage src="https://nicholasgriffin.dev/avatar.png" />
+                  <AvatarFallback>NG</AvatarFallback>
+                </Avatar>
+              </div>
+            </div>
+          </header>
+          {children}
+          <footer className="w-full py-4 px-4 sm:px-6 lg:px-8">
+            <div className="flex items-center justify-center gap-2">
+              <span className="text-muted-foreground text-xs">
+                Created by{' '}
+                <a href="https://nicholasgriffin.dev">Nicholas Griffin</a>
+              </span>
+            </div>
+          </footer>
+        </div>
+      </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,17 @@
 export default function Home() {
   return (
-    <main>
-      <h1>Hello World!</h1>
+    <main className="p-5 bg-white dark:bg-gray-900 antialiased">
+      <div className="flex flex-col">
+        <div className="w-full max-w-3xl py-4 px-4 sm:px-6 lg:px-8 m-auto items-center text-center">
+          <div
+            className={`transition-all duration-500 ease-in-out mt-[5vh] lg:mt-[15vh]`}
+          >
+            <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
+              Hello World!
+            </h1>
+          </div>
+        </div>
+      </div>
     </main>
   );
 }
